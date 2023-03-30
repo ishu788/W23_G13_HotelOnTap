715d9299d27988aed2b878fe624a1a900c8879b8
ishu788 <ishu788.is@gmail.com>
2023-03-30 07:43:31 -0700

    Login/signup Authentication Some ui changes

--- app/src/main/java/com/example/tryingapplication/splash.java
+++ app/src/main/java/com/example/tryingapplication/splash.java
@@ -1,11 +1,11 @@
 package com.example.tryingapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
 
@@ -20,7 +20,7 @@ public class splash extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(),MainActivity.class);
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
                 v.getContext().startActivity(intent);
             }
         });
