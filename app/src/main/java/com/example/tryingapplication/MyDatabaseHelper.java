fbd8f27d205adcff165a306026b23652d3e930ee
ishu788 <ishu788.is@gmail.com>
2023-04-09 04:22:21 -0700

    Database update

---
+++ app/src/main/java/com/example/tryingapplication/MyDatabaseHelper.java
@@ -0,0 +1,17 @@
+//package com.example.tryingapplication;
+//
+//
+//import android.database.sqlite.SQLiteDatabase;
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class MyDatabaseHelper  extends SQLiteOpenHelper {
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//
+//    }
+//}
